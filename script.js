6bdfc62030c0eba9f1ee4518e09f011b52b6dec9
Quoc2k5 <fxnvquoc19052k5@gmail.com>
2024-11-16 00:51:44 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,85 @@
+// Tìm kiếm
+let menu = document.querySelector('#menu-bars');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () =>{
+  menu.classList.toggle('fa-times');
+  navbar.classList.toggle('active');
+}
+
+window.onscroll = () =>{
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+}
+
+document.querySelector('#search-icon').onclick = () =>{
+  document.querySelector('#search-form').classList.toggle('active');
+}
+
+document.querySelector('#close').onclick = () =>{
+  document.querySelector('#search-form').classList.remove('active');
+}
+
+var swiper = new Swiper(".home-slider", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 7500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  loop:true,
+});
+var swiper = new Swiper(".review-slider", {
+  loop:true,
+  spaceBetween: 20,
+  autoplay: {
+      delay: 7500,
+      disableOnInteraction: true,
+  },
+  centeredSlides: false,
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1020: {
+      slidesPerView: 3,
+    },
+  },
+});
+
+// Comment
+const next = document.querySelector('.next')
+const prev = document.querySelector('.prev')
+const comment = document.querySeletor('#list-comment')
+const commentItem = document.querySelectorAll('#list-comment .item')
+var translateY = 0
+var count = commentItem.length
+
+next.addEventListener('click', function (event) {
+  event.preventDefault()
+  if (count == 1) {
+    // Xem hết bình luận
+    return false
+  }
+  translateY += -400
+  comment.style.transform = `translateY(${translateY}px)`
+  count--
+})
+
+prev.addEventListener('click', function (event) {
+  event.preventDefault()
+  if (count == 3) {
+    // Xem hết bình luận
+    return false
+  }
+  translateY += 400
+  comment.style.transform = `translateY(${translateY}px)`
+  count++
+})
